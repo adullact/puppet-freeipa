6a3aca54900d9297e33aba17684a2a5435480654
Scott Barthelemy <scott@sudokeys.com>
2018-07-25 10:34:11 +0200

    Clean spec_helper_acceptance file

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -44,44 +44,6 @@ RSpec.configure do |c|
 ### * IPV6 activée
 
 
-#    #we need :
-#    # * an already existing system user to test backup without root privileges
-#    # * a restic wrapper with old path used before 0.10
-#    pp = <<-EOS
-#      accounts::user { 'bar': 
-#      }
-#      ->
-#      exec { 'produce ssh pair of keys' :
-#        command => '/usr/bin/ssh-keygen -t rsa -f /home/bar/.ssh/id_rsa -q -P ""',
-#        user    => 'bar',
-#	creates => '/home/bar/.ssh/id_rsa',
-#      }
-#      ->
-#      exec { 'set authorized_keys' :
-#        command => '/bin/cp -af /home/bar/.ssh/id_rsa.pub /home/bar/.ssh/authorized_keys',
-#        user    => 'bar',
-#	unless  => '/usr/bin/test -s /home/bar/.ssh/authorized_keys',
-#      }
-#      ->
-#      file { '/usr/local/bin/restic-foowrapper-bar' :
-#        ensure  => file,
-#	content => 'testing file to similate old wrapper used with bumpversion true\n',
-#      }
-#    EOS
-#
-#    apply_manifest_on(agents, pp, catch_failures: true)
-
   end
 
 end
-
-#shared_examples 'a idempotent resource' do
-#  it 'applies with no errors' do
-#    apply_manifest(pp, catch_failures: true)
-#  end
-#
-#  it 'applies a second time without changes' do
-#    apply_manifest(pp, catch_changes: true)
-#  end
-#end
-
