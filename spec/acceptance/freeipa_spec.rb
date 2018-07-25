4d3adde3d7817f67a89288c15b4dba6311eb2ff4
Scott Barthelemy <scott@sudokeys.com>
2018-07-25 17:46:20 +0200

    Update acceptance test calling freeipa  with default parameters set

--- spec/acceptance/freeipa_spec.rb
+++ spec/acceptance/freeipa_spec.rb
@@ -5,14 +5,33 @@ describe ' freeipa class' do
   context 'master' do
     context 'with default parameters' do
       it 'applies idempotently' do
-        pp = "class { 'freeipa': }"
+        pp = <<-EOS
+class { 'freeipa':
+  ipa_role => 'master',
+  hostname => 'ipa-server-1',
+  domain => 'vagrant.example.lan',
+  ipa_server_fqdn => 'ipa-server-1.vagrant.example.lan',
+  admin_password => 'vagrant123',
+  directory_services_password => 'vagrant123',
+  install_ipa_server => true,
+  ip_address => '192.168.44.35',
+  enable_ip_address => true,
+  enable_hostname => true,
+  manage_host_entry => true,
+  install_epel => true,
+  webui_disable_kerberos => true,
+  webui_enable_proxy => true,
+  webui_force_https => true,
+}
+        EOS
+
         apply_manifest(pp, :catch_failures => true)
         apply_manifest(pp, :catch_changes  => true)
-      end  
-
-      step 'test ipactl status with beaker exec' do
-        on(host, "ipactl status")
       end
+
+#      step 'test ipactl status with beaker exec' do
+#        on(host, "ipactl status")
+#      end
     end
   end
 
@@ -21,7 +40,7 @@ describe ' freeipa class' do
 #      pp = "class { 'freeipa': }"
 #      apply_manifest(pp, :catch_failures => true)
 #      apply_manifest(pp, :catch_changes  => true)
-#    
+#
 #    end
 #
 #
@@ -36,7 +55,7 @@ describe ' freeipa class' do
 #      pp = "class { 'freeipa': }"
 #      apply_manifest(pp, :catch_failures => true)
 #      apply_manifest(pp, :catch_changes  => true)
-#    
+#
 #    end
 #
 #
